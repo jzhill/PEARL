9e14946df40272dea75e66a542d191b87ab2baee
Jeremy Hill <jeremy@hill.net.au>
2025-02-17 07:51:59 +1200

    Initial R script and renv lock

---
+++ R/01_load_data.R
@@ -0,0 +1,14 @@
+# Title and description --------------------------------------------
+
+# Loading PEARL data from REDCap
+# Data are owned by University of Sydney and Kiribati MHMS
+
+# Author:           Jeremy Hill
+# Date commenced:   16 Feb 2025
+
+
+# Packages -----------------------------------------
+
+library(tidyverse)
+library(here)
+library(REDCapR)
