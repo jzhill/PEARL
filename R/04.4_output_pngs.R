d6e14925f0ff09358db1f3df9abd10dc0420e3ff
Jeremy Hill <jeremy@hill.net.au>
2025-03-16 12:00:21 +1200

    Updated to make this script work. Make an output of all plot_ map_ and table_ objects in a dated folder

--- R/04.4_output_pngs.R
+++ R/04.4_output_pngs.R
@@ -3,60 +3,58 @@ library(lubridate)
 library(here)
 library(fs)
 library(flextable)
-library(webshot2)
+
+# Clear existing objects from previous runs
+rm(list = ls(pattern = "^(plot_|table_|map_)", envir = .GlobalEnv))
 
 # Set today's date
 report_date <- Sys.Date()
-report_dir <- here("reports", paste0("PEARL_Sitrep_", report_date))
+report_dir <- here("figures", paste0("Outputs_", report_date))
 
 # Create directory if it doesn't exist
 if (!dir_exists(report_dir)) {
   dir_create(report_dir)
 }
 
-# List of scripts to run (located in ~/R with prefixes 05 or 06)
-scripts_to_run <- list.files(here("R"), pattern = "^(05|06)_.*\\.R$", full.names = TRUE)
+# Run all relevant scripts from ~/R with prefixes 05 or 06
+scripts_to_run <- list.files(here("R"), pattern = "^(05|06).*\\.R$", full.names = TRUE)
+lapply(scripts_to_run, source)
 
-# Source all the scripts
-for (script in scripts_to_run) {
-  source(script)
+# Function to save plots
+save_plot <- function(plot, filename) {
+  ggsave(file.path(report_dir, paste0(filename, "_", format(Sys.Date(), "%Y%m%d"), ".png")), 
+         plot, width = 8, height = 6, dpi = 300)
+  message("Saved: ", filename)
 }
 
-# Function to save a plot with timestamp
-save_plot_with_date <- function(plot, filename) {
-  timestamp <- format(Sys.time(), "%Y%m%d_%H%M%S")
-  filepath <- file.path(report_dir, paste0(filename, "_", timestamp, ".png"))
-  ggsave(filepath, plot, width = 8, height = 6, dpi = 300)
-  message("Saved: ", filepath)
+# Function to save flextable as an image
+save_table <- function(table, filename) {
+  if (inherits(table, "flextable")) {
+    flextable::save_as_image(table, 
+                             path = file.path(report_dir, paste0(filename, "_", format(Sys.Date(), "%Y%m%d"), ".png")))
+    message("Saved: ", filename)
+  }
 }
 
-# Function to save flextable as an image without keeping the HTML file
-save_table_as_image <- function(table, filename) {
-  timestamp <- format(Sys.time(), "%Y%m%d_%H%M%S")
-  html_path <- tempfile(fileext = ".html")  # Temporary HTML file
-  image_path <- file.path(report_dir, paste0(filename, "_", timestamp, ".png"))
-  save_as_html(table, path = html_path)
-  webshot2::webshot(html_path, image_path)
-  unlink(html_path)  # Delete the temporary HTML file
-  message("Saved: ", image_path)
-}
-
-# Identify all objects in the environment that match 'plot_', 'map_', or 'table_'
+# Identify objects matching 'plot_', 'map_', or 'table_'
 all_objects <- ls()
 plot_objects <- grep("^plot_", all_objects, value = TRUE)
 map_objects <- grep("^map_", all_objects, value = TRUE)
 table_objects <- grep("^table_", all_objects, value = TRUE)
 
-# Save all identified plot and map objects
-for (plot_name in c(plot_objects, map_objects)) {
-  plot_obj <- get(plot_name)
-  save_plot_with_date(plot_obj, plot_name)
+# Save only identified plot, map, and table objects
+for (obj in c(plot_objects, map_objects)) {
+  obj_value <- get(obj)
+  if (inherits(obj_value, "ggplot")) {
+    save_plot(obj_value, obj)
+  }
 }
 
-# Save tables as images
-for (table_name in table_objects) {
-  table_obj <- get(table_name)
-  save_table_as_image(table_obj, table_name)
+for (obj in table_objects) {
+  obj_value <- get(obj)
+  if (inherits(obj_value, "flextable")) {
+    save_table(obj_value, obj)
+  }
 }
 
 message("All reports generated and saved in: ", report_dir)
